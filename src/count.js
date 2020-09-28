38dd3b232660135c6bd9d0ba0416dcd3521e246c
fekalegi <fekalegi@gmail.com>
2020-09-28 20:40:20 +0700

    finishing all the function

--- src/count.js
+++ src/count.js
@@ -5,41 +5,53 @@ class count extends Component {
   state = {
     hitung: 0,
     variant: "warning",
+    value: null,
   };
 
   addCount = () => {
-    if (this.state.hitung <= 0) {
-      return null;
+    if (this.state.hitung < 0) {
+      this.setState({
+        hitung: "ZERO",
+      });
     } else {
       this.setState({
-        hitung: this.setState.hitung + 1,
+        variant: "dark",
+        hitung: this.state.hitung + 1,
       });
     }
+    this.cekCount();
   };
 
   cekCount = () => {
     if (this.state.hitung === 0) {
       this.setState({
         variant: "warning",
+        value: "ZERO",
       });
-      return "ZERO";
     } else {
       this.setState({
         variant: "dark",
+        value: this.state.hitung + 1,
       });
-      return this.state.hitung;
     }
   };
+
+  componentDidMount() {
+    this.cekCount();
+  }
 
   render() {
     return (
       <Container fluid>
         <Row xs={2} md={4} lg={6}>
           <Col>
-            <Button variant={this.state.variant}> {this.cekCount}</Button>
+            <Button variant={this.state.variant}> {this.state.value} </Button>
           </Col>
           <Col>
-            <Button variant="primary"> + </Button>
+            <Button variant="primary" onClick={this.addCount}>
+              {" "}
+              +{" "}
+            </Button>
           </Col>
         </Row>
       </Container>
